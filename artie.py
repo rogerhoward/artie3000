c8fc838a416443816027499a9935040fc2110498
Roger Howard <rogerhoward@mac.com>
2019-04-16 14:22:53 -0400

    check

---
+++ artie.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import asyncio
+import websockets
+import random, string
+import simplejson as json
+
+
+def random_id(len=10):
+    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=len))
+
+class Artie(object):
+    def __init__():
+        pass
+
+async def bot():
+    async with websockets.connect(
+            'ws://local.codewithartie.com:8899/websocket') as websocket:
+        # name = input("What's your name? ")
+        msg_id = random_id()
+        vers = {"cmd": "version", "id": msg_id}
+
+        await websocket.send(json.dumps(vers))
+        print(f"> {vers}")
+
+        greeting = await websocket.recv()
+        print(f"< {greeting}")
+
+asyncio.get_event_loop().run_until_complete(bot())
